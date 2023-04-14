b190d76df811cdcaa5c1a0c6324cf1f6d285e93f
Marcos <71572572+spalheapp@users.noreply.github.com>
2023-04-14 13:03:42 -0300

    Add files via upload

--- scripts/whatsapp/alugue.js
+++ scripts/whatsapp/alugue.js
@@ -1,9 +1,9 @@
 const nome = document.querySelector('#nome')
-const endereco = document.querySelector('#cidade')
+const endereco = document.querySelector('#endereco')
 const tipoDeImovel = document.querySelector('#tipo')
 const telefone = document.querySelector('#numero')
-const quantidade = document.querySelector('#instalação')
+const quantidade = document.querySelector('#quantidade')
 const email = document.querySelector('#email')
 
 function simular() {
-    window.location.href = `https://api.whatsapp.com/send?phone=558199701949&text=Nome:%20${nome.value}%Cidade:%20${endereco.value}%0ATipo:%20${tipoDeImovel.value}%0AContato:%20${telefone.value}%Instalação:%20${quantidade.value}%0AE-mail:%20${email.value}`}
+    window.location.href = `https://api.whatsapp.com/send?phone=5581999701949&text=Nome:%20${nome.value}%20Cidade:%20${endereco.value}%20Tipo%20de%20instalação:%20${tipoDeImovel.value}%20Contato:%20${telefone.value}%20Quantidade%20de%20instalação:%20${quantidade.value}%0AE-mail:%20${email.value}`}
