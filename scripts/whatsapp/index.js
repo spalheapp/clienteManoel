775fd14f3f1384e1fcca1cc8813a42e27c4a9f53
Marcos <71572572+spalheapp@users.noreply.github.com>
2023-04-14 13:46:03 -0300

    Add files via upload

--- scripts/whatsapp/index.js
+++ scripts/whatsapp/index.js
@@ -1,8 +1,9 @@
-const button = document.querySelector('#reservaButton')
+const button = document.querySelector('#optionsButton')
 const dataEntrada = document.querySelector('#dataEntrada')
 const dataSaida = document.querySelector('#dataSaida')
 const adulto = document.querySelector('#nome')
 const crianca = document.querySelector('#whatsapp')
+const selecionar = document.querySelector('#selecionar')
 const lugar = document.querySelector('#lugares')
 
 
@@ -12,7 +13,7 @@ function reserva(){
     const entrada = dataEntrada.value.split("-").reverse().join().replace(/,/g, "/")
     const saida = dataSaida.value.split("-").reverse().join().replace(/,/g, "/")
 
-    window.location.href = `https://api.whatsapp.com/send?phone=5581999701949&text=Opção:%20${lugar.value}%0AData%20de%20visita:%20${entrada}%0APrazo%20de%20entrega:%20${saida}%0ANome:%20${adulto.value}%0AWhatsApp:%20${crianca.value}`
+    window.location.href = `https://api.whatsapp.com/send?phone=5581999701949&text=Tipo%20de%20telhado:%20${selecionar.value}%Categoria%20de%consumo:%20${lugar.value}%0AData%20de%20visita:%20${entrada}%0APrazo%20de%20entrega:%20${saida}%0ANome:%20${adulto.value}%0AWhatsApp:%20${crianca.value}`
 }
 
 
